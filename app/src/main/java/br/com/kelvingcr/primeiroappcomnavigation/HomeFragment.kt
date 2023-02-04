d3c2acf3b72aa0df957c96a71549cf6b700d6623
Kelvin Rodrigues <jedsonsougato15@gmail.com>
2023-02-03 21:14:19 -0300

    teste branch

--- app/src/main/java/br/com/kelvingcr/primeiroappcomnavigation/HomeFragment.kt
+++ app/src/main/java/br/com/kelvingcr/primeiroappcomnavigation/HomeFragment.kt
@@ -22,6 +22,7 @@ class HomeFragment : Fragment() {
 
         //Usando um facilitador
         //Commit
+        //Branch
         binding.btnSignIn.setOnClickListener(
             Navigation.createNavigateOnClickListener(R.id.action_homeFragment_to_loginFragment)
         )
